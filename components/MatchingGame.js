090e0667b720389cb65e9a736f1fd7ddd56aa3c1
piamsak <piamsakboat@gmail.com>
2021-04-28 04:12:55 +0700

    be able to play the game

--- components/MatchingGame.js
+++ components/MatchingGame.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import CardGame from './CardGame';
 
 const MatchingGame = ({ cards }) => {
     const [openCard, setOpenCard] = useState([]);
+    const [matched, setMatched] = useState([]);
 
     // make a pair of cards
     const pairOfCards = [...cards, ...cards];
+
+    useEffect(() => {
+        const firstSelected = pairOfCards[openCard[0]];
+        const secondSelected = pairOfCards[openCard[1]];
+
+        if(secondSelected && firstSelected.id === secondSelected.id) {
+            setMatched([...matched, firstSelected.id]);
+        }
+
+        if(openCard.length === 2) {
+            setTimeout(() => {
+                setOpenCard([]);
+            }, 1000);
+        }
+    }, [openCard])
 
     const handleFlip = index => {
         setOpenCard(opened => [...opened, index]);
@@ -20,6 +35,7 @@ const MatchingGame = ({ cards }) => {
                         pairOfCards.map((card, index) => {
                             let flipped = false;
                             if( openCard.includes(index) ) flipped = true;
+                            if( matched.includes(card.id) ) flipped = true;
 
                             return <CardGame index={index} flipped={flipped} cardIcon={card.icon} handleFlip={handleFlip} />
                         })
