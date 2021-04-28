429e117c1f9570efbbdf2750d32adbb7a2e555fa
piamsak <piamsakboat@gmail.com>
2021-04-28 15:09:12 +0700

    remove api key

--- pages/api/openweather.js
+++ pages/api/openweather.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://api.openweathermap.org',
+    baseURL: 'https://api.openweathermap.org',
     params: {
-        appid: 'ed7ddbf6484f7a9102f78883f5e91458'
+        appid: 'your api key'
     }
 });
